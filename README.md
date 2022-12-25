37a60cd73067a7d2443ecf891140500a54755556
Andre Freitas <andrecfreitas58@gmail.com>
2022-12-25 00:49:24 +0000

    refactor: refactor whole extension for scalability

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
-# About  
-This extension gives you the option to shutdown. reboot, suspend, hibernate and logout your pc.
-It also have an option to change the icon of the options to fit your theme.
+# ulauncher-session-manager  
 
-If you need an option to logout to your desktop environment just add an issue and i will see what i can do :D
+> [Ulauncher](https://ulauncher.io) extension for controling system session options
 
-![image](https://user-images.githubusercontent.com/35658492/205598826-90f6c225-6ab4-4ae7-9127-f6284ad252da.png)
+# Requirements
 
-
-# Dependencies
-- Systemd
-
+- Ulauncher
+- Python 3
 
 # Installation
+
 Just go to ulauncher extensions tab and paste the repo link to it.
 
-    https://github.com/lighttigerXIV/ulauncher-session-manager
+```
+    https://github.com/andresatierf/ulauncher-session-manager
+```
+
+# Authors
+- [Andre Freitas](https://github.com/andresatierf)
+
+# Icons
+- Icons created by SumberRejeki - Flaticon
+
+## License
+
+MIT &copy; [Andre Freitas]
--- main.py
+++ main.py
@@ -1,85 +1,75 @@
-from asyncio import subprocess
-from dataclasses import dataclass
+import subprocess
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
-from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-
-import subprocess
 
 
-class Terminal_Runner(Extension):
-
+class SessionManager(Extension):
     def __init__(self):
         super().__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-        self.subscribe(ItemEnterEvent, ExecuteSession())
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
 
 class KeywordQueryEventListener(EventListener):
-
     def on_event(self, event, extension):
+        options = [
+            {
+                "name": "Lock",
+                "icon": "lock.png",
+                "cmd": "xdg-screensaver lock",
+            },
+            {
+                "name": "Log Out",
+                "icon": "logout.png",
+                "cmd": "gnome-session-quit --no-prompt",
+            },
+            {
+                "name": "Suspend",
+                "icon": "suspend.png",
+                "cmd": "systemctl suspend",
+            },
+            {
+                "name": "Restart",
+                "icon": "reboot.png",
+                "cmd": "systemctl reboot",
+            },
+            {
+                "name": "Power Off",
+                "icon": "poweroff.png",
+                "cmd": "systemctl poweroff",
+            },
+        ]
+
+        query = event.get_argument()
+        if query:
+            options = list(filter(lambda option: query.lower() in option['name'].lower(), options))
+
+        return RenderResultListAction(
+            list(
+                map(
+                    lambda option: ExtensionResultItem(
+                        icon=f"images/{option['icon']}",
+                        name=option["name"],
+                        on_enter=ExtensionCustomAction(
+                            option["cmd"],
+                            keep_app_open=False,
+                        ),
+                    ),
+                    options,
+                )
+            )
+        )
 
 
-        options = []
-
-
-        for i in range(5):
-
-            data = { "option": i }
-
-            iconStyle = extension.preferences["icon"]
-
-            if iconStyle == "color": optionIcon = "images/icon.png"
-            if iconStyle == "black": optionIcon = "images/icon-black.png"
-            if iconStyle == "white": optionIcon = "images/icon-white.png"
-
-            if i == 0: optionName = "Shutdown"
-
-            if i == 1: optionName = "Reboot"
-
-            if i == 2: optionName = "Suspend"
-
-            if i == 3: optionName = "Hibernate"
-
-            if i == 4: optionName = "Logout"
-
-
-            options.append(ExtensionResultItem(icon=optionIcon,
-                                                    name=optionName,
-                                                    on_enter=ExtensionCustomAction(data, keep_app_open=True)))
-
-        return RenderResultListAction(options)
-
-
-class ExecuteSession(EventListener):
-
+class ItemEnterEventListener(EventListener):
     def on_event(self, event, extension):
-
         data = event.get_data()
-        
-        option = data["option"]
+        subprocess.run(data, shell=True)
 
 
-        if option == 0: command = "systemctl poweroff"
-        if option == 1: command = "systemctl reboot"
-        if option == 2: command = "systemctl suspend"
-        if option == 3: command = "systemctl hibernate"
-        if option == 4:
-
-            desktopEnvironment = extension.preferences["desktop-environment"]
-
-            if(desktopEnvironment == "gnome"): command = "gnome-session-quit --no-prompt"
-            if(desktopEnvironment == "kde"): command = "qdbus org.kde.ksmserver /KSMServer logout 0 0 1"
-
-        subprocess.run( [command], shell=True )
-
-        return HideWindowAction()
-
-
-
-if __name__ == '__main__':
-    Terminal_Runner().run()
+if __name__ == "__main__":
+    SessionManager().run()
